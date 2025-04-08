0eafddd50720a2f1c013ec807833b08109a25366
Duong Nhat <duongnhat512@gmail.com>
2025-04-08 17:00:08 +0700

    customer service

--- customer-service/src/app.js
+++ customer-service/src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
-app.use('/api/v1/products', require('./routes/product.route'));
+app.use('/api/v1/customers', require('./routes/customer.route'));
+
+ module.exports = app;
